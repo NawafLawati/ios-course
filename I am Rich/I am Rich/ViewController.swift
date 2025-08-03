b79708951ed41d4e248f7eda6f7b78491f013565
Nawaf Al Lawati <nawaf.lawati@gmail.com>
2025-08-03 15:29:06 +0400

    I am Rich and I am Poor (Sections 1,2,3)

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Nawaf Al Lawati on 03/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
